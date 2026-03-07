aa64ca2fd8da94ba42fa9387d17c812fbe722d9f
zcq <2928235428@qq.com>
2026-03-07 23:25:49 +0800

    NJU-ProjectN/navy-apps ics2024 initialized
    NJU-ProjectN/navy-apps d12fc38844a909502e9270686c04a577a0b85526 Makefile: force to run submake for archives to keep them up-to-date

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,11 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    default: panic("Unhandled event ID = %d", e.event);
+    case EVENT_YIELD:
+      printf("nanos-lite: handle EVENT_YIELD\n");
+      break;
+    default:
+      panic("Unhandled event ID = %d", e.event);
   }
 
   return c;
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,49 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+
+#if defined(__ISA_AM_NATIVE__)
+# define EXPECT_TYPE EM_X86_64
+#elif defined(__ISA_X86__)
+# define EXPECT_TYPE EM_386
+#elif defined(__ISA_MIPS32__)
+# define EXPECT_TYPE EM_MIPS
+#elif defined(__ISA_RISCV32__) || defined(__ISA_RISCV64__)
+# define EXPECT_TYPE EM_RISCV
+#elif defined(__ISA_LOONGARCH32R__)
+# ifdef EM_LOONGARCH
+#  define EXPECT_TYPE EM_LOONGARCH
+# else
+#  error Unsupported ISA
+# endif
+#else
+# error Unsupported ISA
+#endif
+
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  (void)pcb;
+  (void)filename;
+
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(ehdr));
+
+  assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
+  assert(ehdr.e_machine == EXPECT_TYPE);
+
+  for (int i = 0; i < ehdr.e_phnum; i++) {
+    Elf_Phdr phdr;
+    size_t off = ehdr.e_phoff + i * ehdr.e_phentsize;
+    ramdisk_read(&phdr, off, sizeof(phdr));
+
+    if (phdr.p_type != PT_LOAD) continue;
+
+    ramdisk_read((void *)phdr.p_vaddr, phdr.p_offset, phdr.p_filesz);
+    memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz);
+  }
+
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
@@ -19,4 +59,3 @@ void naive_uload(PCB *pcb, const char *filename) {
   Log("Jump to entry = %p", entry);
   ((void(*)())entry) ();
 }
-
